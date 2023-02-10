3a2bb1d1afe3e4fb57e0e70803ef0137196b7652
Hafiidz <3688500+Hafiidz@users.noreply.github.com>
2023-02-10 10:07:00 +0800

    rewrite path helper

--- scripts/coord_helper.py
+++ scripts/coord_helper.py
@@ -1,9 +1,11 @@
 from numpy import exp, abs, angle
 import pandas as pd
 
+p = "/root/catkin_ws/src/swarm/scripts/"
+# p = "~/catkin_ws/src/swarm/scripts/"
+
 
 def shape(s):
-    p = "/root/catkin_ws/src/swarm/scripts/"
     df = pd.read_csv("{}{}.csv".format(p, s))
 
     # coordinate is 11x11, normalize to ensure, 3 spaces to the side and 5 at center
@@ -57,3 +59,53 @@ def coord_shifted(coord, shift=1):
     idx = l[-shift:] + l[0:-shift]
     df = df.iloc[idx]
     return df[["x", "y"]].to_numpy()
+
+
+def shape_df(s):
+    df = pd.read_csv("{}{}.csv".format(p, s))
+
+    # coordinate is 11x11, normalize to ensure, 3 spaces to the side and 5 at center
+    target_space = 3
+    target_length = 5
+
+    df["x"] = df["x"] - df["x"].min()
+    df["y"] = df["y"] - df["y"].min()
+
+    l_x = abs(df["x"].max() - df["x"].min())
+    l_y = abs(df["y"].max() - df["y"].min())
+
+    length = max(l_x, l_y)
+
+    df = (df / length * target_length) + target_space
+    return df.round(decimals=4)
+
+
+def coord_sorted_df(s, sort="polar", offset=-5.5):
+    goal = shape_df(s)
+
+    df = pd.DataFrame(goal, columns=["x", "y"])
+    df["r"], df["d"] = xy2polar(df.x + offset, df.y + offset)
+
+    if sort == "polar":
+        df.sort_values(by="d", inplace=True)
+    else:
+        df.sort_values(by=sort, inplace=True)
+
+    return df
+
+
+def coord_shifted_df(df, shift=1):
+    l = df.index.to_list()
+    idx = l[-shift:] + l[0:-shift]
+    df = df.iloc[idx]
+    return df
+
+
+def gen_df_list():
+    df1 = coord_sorted_df("square")
+    df2 = coord_sorted_df("circle")
+    df3 = coord_shifted_df(df2)
+    df4 = coord_sorted_df("square")
+    df5 = coord_shifted_df(df4, 10)
+
+    return [df1, df2, df3, df4, df5]
--- scripts/path_helper.py
+++ scripts/path_helper.py
@@ -1,19 +1,7 @@
-"""
-
-Path planning with Bezier curve.
-
-author: Atsushi Sakai(@Atsushi_twi)
-
-"""
-
-import matplotlib.pyplot as plt
-import numpy as np
-import scipy.special
-
-show_animation = True
+from bezier_path import *
 
 
-def calc_4points_bezier_path(sx, sy, syaw, ex, ey, eyaw, offset):
+def calc_bezier_path_n_points(sx, sy, syaw, ex, ey, eyaw, n_points=10, offset=1):
     """
     Compute control points and path given start and end position.
 
@@ -36,194 +24,30 @@ def calc_4points_bezier_path(sx, sy, syaw, ex, ey, eyaw, offset):
         ]
     )
 
-    path = calc_bezier_path(control_points, n_points=100)
-
-    return path, control_points
+    return calc_bezier_path(control_points, n_points)
 
 
-def calc_bezier_path(control_points, n_points=100):
-    """
-    Compute bezier path (trajectory) given control points.
+def calc_path_from_df_list(df_list):
+    l = []
+    for d in df_list:
+        l.append(d.shape[0])
 
-    :param control_points: (numpy array)
-    :param n_points: (int) number of points in the trajectory
-    :return: (numpy array)
-    """
-    traj = []
-    for t in np.linspace(0, 1, n_points):
-        traj.append(bezier(t, control_points))
+    length = min(l)
 
-    return np.array(traj)
+    path_x = pd.DataFrame(range(1, length + 1), columns=["turtle"])
+    path_y = pd.DataFrame(range(1, length + 1), columns=["turtle"])
 
+    for i in range(length):
+        for j in range(len(df_list) - 1):
+            current = df_list[j].iloc[i]
+            target = df_list[j + 1].iloc[i]
 
-def bernstein_poly(n, i, t):
-    """
-    Bernstein polynom.
+            p2 = calc_bezier_path_n_points(
+                current.x, current.y, current.d, target.x, target.y, target.d
+            )
 
-    :param n: (int) polynom degree
-    :param i: (int)
-    :param t: (float)
-    :return: (float)
-    """
-    return scipy.special.comb(n, i) * t**i * (1 - t) ** (n - i)
+            for k in range(len(p2)):
+                path_x.at[i, str(j) + "_" + str(k)] = p2[k][0]
+                path_y.at[i, str(j) + "_" + str(k)] = p2[k][1]
 
-
-def bezier(t, control_points):
-    """
-    Return one point on the bezier curve.
-
-    :param t: (float) number in [0, 1]
-    :param control_points: (numpy array)
-    :return: (numpy array) Coordinates of the point
-    """
-    n = len(control_points) - 1
-    return np.sum(
-        [bernstein_poly(n, i, t) * control_points[i] for i in range(n + 1)], axis=0
-    )
-
-
-def bezier_derivatives_control_points(control_points, n_derivatives):
-    """
-    Compute control points of the successive derivatives of a given bezier curve.
-
-    A derivative of a bezier curve is a bezier curve.
-    See https://pomax.github.io/bezierinfo/#derivatives
-    for detailed explanations
-
-    :param control_points: (numpy array)
-    :param n_derivatives: (int)
-    e.g., n_derivatives=2 -> compute control points for first and second derivatives
-    :return: ([numpy array])
-    """
-    w = {0: control_points}
-    for i in range(n_derivatives):
-        n = len(w[i])
-        w[i + 1] = np.array([(n - 1) * (w[i][j + 1] - w[i][j]) for j in range(n - 1)])
-    return w
-
-
-def curvature(dx, dy, ddx, ddy):
-    """
-    Compute curvature at one point given first and second derivatives.
-
-    :param dx: (float) First derivative along x axis
-    :param dy: (float)
-    :param ddx: (float) Second derivative along x axis
-    :param ddy: (float)
-    :return: (float)
-    """
-    return (dx * ddy - dy * ddx) / (dx**2 + dy**2) ** (3 / 2)
-
-
-def plot_arrow(x, y, yaw, length=1.0, width=0.5, fc="r", ec="k"):  # pragma: no cover
-    """Plot arrow."""
-    if not isinstance(x, float):
-        for ix, iy, iyaw in zip(x, y, yaw):
-            plot_arrow(ix, iy, iyaw)
-    else:
-        plt.arrow(
-            x,
-            y,
-            length * np.cos(yaw),
-            length * np.sin(yaw),
-            fc=fc,
-            ec=ec,
-            head_width=width,
-            head_length=width,
-        )
-        plt.plot(x, y)
-
-
-def main():
-    """Plot an example bezier curve."""
-    start_x = 10.0  # [m]
-    start_y = 1.0  # [m]
-    start_yaw = np.radians(180.0)  # [rad]
-
-    end_x = -0.0  # [m]
-    end_y = -3.0  # [m]
-    end_yaw = np.radians(-45.0)  # [rad]
-    offset = 3.0
-
-    path, control_points = calc_4points_bezier_path(
-        start_x, start_y, start_yaw, end_x, end_y, end_yaw, offset
-    )
-
-    # Note: alternatively, instead of specifying start and end position
-    # you can directly define n control points and compute the path:
-    # control_points = np.array([[5., 1.], [-2.78, 1.], [-11.5, -4.5], [-6., -8.]])
-    # path = calc_bezier_path(control_points, n_points=100)
-
-    # Display the tangent, normal and radius of cruvature at a given point
-    t = 0.86  # Number in [0, 1]
-    x_target, y_target = bezier(t, control_points)
-    derivatives_cp = bezier_derivatives_control_points(control_points, 2)
-    point = bezier(t, control_points)
-    dt = bezier(t, derivatives_cp[1])
-    ddt = bezier(t, derivatives_cp[2])
-    # Radius of curvature
-    radius = 1 / curvature(dt[0], dt[1], ddt[0], ddt[1])
-    # Normalize derivative
-    dt /= np.linalg.norm(dt, 2)
-    tangent = np.array([point, point + dt])
-    normal = np.array([point, point + [-dt[1], dt[0]]])
-    curvature_center = point + np.array([-dt[1], dt[0]]) * radius
-    circle = plt.Circle(
-        tuple(curvature_center), radius, color=(0, 0.8, 0.8), fill=False, linewidth=1
-    )
-
-    assert path.T[0][0] == start_x, "path is invalid"
-    assert path.T[1][0] == start_y, "path is invalid"
-    assert path.T[0][-1] == end_x, "path is invalid"
-    assert path.T[1][-1] == end_y, "path is invalid"
-
-    if show_animation:  # pragma: no cover
-        fig, ax = plt.subplots()
-        ax.plot(path.T[0], path.T[1], label="Bezier Path")
-        ax.plot(control_points.T[0], control_points.T[1], "--o", label="Control Points")
-        ax.plot(x_target, y_target)
-        ax.plot(tangent[:, 0], tangent[:, 1], label="Tangent")
-        ax.plot(normal[:, 0], normal[:, 1], label="Normal")
-        ax.add_artist(circle)
-        plot_arrow(start_x, start_y, start_yaw)
-        plot_arrow(end_x, end_y, end_yaw)
-        ax.legend()
-        ax.axis("equal")
-        ax.grid(True)
-        plt.show()
-
-
-def main2():
-    """Show the effect of the offset."""
-    start_x = 10.0  # [m]
-    start_y = 1.0  # [m]
-    start_yaw = np.radians(180.0)  # [rad]
-
-    end_x = -0.0  # [m]
-    end_y = -3.0  # [m]
-    end_yaw = np.radians(-45.0)  # [rad]
-
-    for offset in np.arange(1.0, 5.0, 1.0):
-        path, control_points = calc_4points_bezier_path(
-            start_x, start_y, start_yaw, end_x, end_y, end_yaw, offset
-        )
-        assert path.T[0][0] == start_x, "path is invalid"
-        assert path.T[1][0] == start_y, "path is invalid"
-        assert path.T[0][-1] == end_x, "path is invalid"
-        assert path.T[1][-1] == end_y, "path is invalid"
-
-        if show_animation:  # pragma: no cover
-            plt.plot(path.T[0], path.T[1], label="Offset=" + str(offset))
-
-    if show_animation:  # pragma: no cover
-        plot_arrow(start_x, start_y, start_yaw)
-        plot_arrow(end_x, end_y, end_yaw)
-        plt.legend()
-        plt.axis("equal")
-        plt.grid(True)
-        plt.show()
-
-
-if __name__ == "__main__":
-    main()
-    #  main2()
+    return path_x, path_y
